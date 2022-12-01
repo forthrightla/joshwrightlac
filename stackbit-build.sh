a0988346c4b7b0a8b2e40f73f5427bb0c608e2d3
Josh <>
2022-12-01 13:02:48 -0600

    Update stackbit-build.sh

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -4,13 +4,4 @@ set -e
 set -o pipefail
 set -v
 
-curl -s -X POST https://api.stackbit.com/project/5e0eb2403bf20d001bb00159/webhook/build/pull > /dev/null
-if [[ -z "${STACKBIT_API_KEY}" ]]; then
-    echo "WARNING: No STACKBIT_API_KEY environment variable set, skipping stackbit-pull"
-else
-    npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5e0eb2403bf20d001bb00159 
-fi
-curl -s -X POST https://api.stackbit.com/project/5e0eb2403bf20d001bb00159/webhook/build/ssgbuild > /dev/null
 jekyll build
-
-curl -s -X POST https://api.stackbit.com/project/5e0eb2403bf20d001bb00159/webhook/build/publish > /dev/null
